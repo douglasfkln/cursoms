d6a93481743aa469f4457e49f26d6f3daf87e38c
Douglas Franklin <douglas.franklin@compasso.com.br>
2020-01-02 16:54:09 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/cursomc2/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.cursomc2.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursomc2.domain.Pedido;
+import com.cursomc2.repositories.PedidoRepository;
+import com.cursomc2.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}	
+}
