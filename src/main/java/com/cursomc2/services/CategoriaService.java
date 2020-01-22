bb04ffac0c6d3b0cf9c5b8cdee58e7d3deddfb14
Douglas Franklin <douglas.franklin@compasso.com.br>
2020-01-22 10:05:22 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/cursomc2/services/CategoriaService.java
+++ src/main/java/com/cursomc2/services/CategoriaService.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 import com.cursomc2.services.exceptions.DataIntegrityException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cursomc2.domain.Categoria;
@@ -45,5 +48,12 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+
 	}
 }
